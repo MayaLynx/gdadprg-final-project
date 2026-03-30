80976ffae4b436c51df4b56e489c8e0f2cc64eb6
MayaLynx <aolay@proton.me>
2026-03-31 03:51:02 +0800

    adjusted player jump strength to be able to reach floating blocks. changed spawn position to be closer to the ground. added makeEnemy definition.

--- GameCharFactory.cpp
+++ GameCharFactory.cpp
@@ -11,17 +11,32 @@ ModularGameObject* GameCharFactory::makePlayer(std::string name, float width, fl
 
   
     // Added the jump and gravity components to the player.
-    player->attachComponent(new JumpComp(500.f));
+    player->attachComponent(new JumpComp(600.f));
     player->attachComponent(new GravityComp(1200.f, 700.f));
 
     player->setMoveSpeed(2.0f);
-    player->setPosition(3, 3);
+    player->setPosition(3, 8);
 
  
     // Player starts ungrounded until collision detects a floor/platform.
     player->setGrounded(false);
 
     return player;
+}
+
+ModularGameObject* GameCharFactory::makeEnemy(std::string name, int xPos, int yPos, float width, float height)
+{
+    ModularGameObject* enemy = new ModularGameObject(name, name + "-1", width, height);
+
+    enemy->attachComponent(new JumpComp(500.f));
+    enemy->attachComponent(new GravityComp(1200.f, 700.f));
+
+    enemy->setMoveSpeed(2.0f);
+    enemy->setMoveDirection(1);
+    enemy->setPosition(xPos, yPos);
+    enemy->setGrounded(false);
+
+    return enemy;
 }
 
 GameObject* GameCharFactory::makeBlock(std::string name, int xPos, int yPos, float width, float height)
